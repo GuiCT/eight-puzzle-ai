b37329d45128ba34236963a383ac8b75db109924
Daniel Serezane <serezane@protonmail.com>
2022-11-26 19:16:05 -0300

    implementação com filas

--- testes/test2.js
+++ testes/test2.js
@@ -1,6 +1,15 @@
-const {MinQueue} = require("heapify");
+let p = [
+    [8,7,0],
+    [2,4,6],
+    [1,5,3]
+];
 
-let obj;
+// Tabuleiro "objetivo" (onde se quer chegar)
+/*let goal = [
+    [1, 2, 3],
+    [5, 6, 0],
+    [7, 8, 4]
+];*/
 
 // Posições objetivo para cada número
 let goal = {
@@ -60,7 +69,7 @@ function isSolvable(b) {
 }
 
 // Obtém todos os movimentos possíveis a partir do tabuleiro atual
-function getPossibleBoards(p) {
+function getPossibleBoards() {
     // Posição do espaço vazio
     let [i, j] = indexOf2D(p, 0);
     // Há métodos mais econômicos de se fazer isso
@@ -104,75 +113,48 @@ function sum_city_block(b) {
     return sum;
 }
 
-function greedy_depth_one(puzzle) {
-    let p = deepCopy2D(puzzle);
-    // Soma dist. city block p/ matriz principal
-    let p_sum = -1;
-    /*
-        É necessário um critério par quebra de loop quando todos
-        os filhos de um nó já foram visitados, mas precisa continuar a busca.
-        Neste caso, "ranqueamos" os nós visitados por nº de visitas e decidimos
-        continuara pusca pelo menos visitado.
-    */
-    let usedBoards = {};
-    let it = 0;
-    while(p_sum != 0) {
-        // Pega os possíveis movimentos
-        const res = getPossibleBoards(p);
-        // Avalia eles por city block
-        const queue = new MinQueue(res.length);
-        for(let i = 0; i < res.length; i++) {
-            queue.push(i, sum_city_block(res[i]));
+/*let res = getPossibleBoards();
+res.forEach(board => {
+    console.log(sum_city_block(board))
+})*/
+
+let p_sum = -1;
+let usedBoards = {};
+let it = 0;
+while(p_sum != 0) {
+    // Pega os possíveis movimentos
+    let res = getPossibleBoards();
+    // Avalia eles por city block
+    let eval = [], next;
+    let stuck = false;
+    res.forEach(board => eval.push(sum_city_block(board)));
+    // Pega o melhor deles
+    // Se já tiver sido utilizado, coloca um valor absurdo no melhor, e tenta de novo
+    next = minFrom(eval);
+    while((usedBoards[res[next].toString()] != undefined) && !stuck){
+        eval[next] = 999;
+        next = minFrom(eval);
+        if(eval[next] == 999){
+            let menosVisitas = 300000; // número propositalmente absurdo
+            stuck = true;
+            for(let i = 0; i < res.length; i++) {
+                if(usedBoards[res[i].toString()] < menosVisitas) {
+                    menosVisitas = usedBoards[res[i].toString()];
+                    next = i;
+                }
+            }
         }
-        // Segunda fila, para possibilidades já usadas. Pode ser necesária.
-        const queueAux = new MinQueue(res.length);
-
-        // Pega o melhor, aparentemente (nível 1), dos possíveis caminhos
-        let next = -1;
-
-        // Se todos os caminhos já foram visitados: desempata.
-        // Critério de desempate: número de visitas * valor do caminho (original, não absurdo)
-        for(let i = 0; i < res.length; i++) {
-            const b_val = queue.peekPriority();
-            next = queue.pop();
-            const r = res[next].toString();
-            if(usedBoards[r] == undefined)
-                break;
-            queueAux.push(i, usedBoards[r] * b_val);
-        }
-
-        // Tudo repetida
-        if(queueAux.size == res.length)
-            next = queueAux.pop()
-
-        // Adiciona o tabuleiro escolhido aos tabuleiros já utilizados
-        const r = res[next].toString();
-        if(usedBoards[r] === undefined){
-            usedBoards[r] = 0;
-        }
-
-        usedBoards[r] += 1;
-        // Troca p pelo escolhido e reavalia a soma
-        p = res[next];
-        p_sum = sum_city_block(p);
-        it += 1;
     }
-
-    return {board: p, it: it};
+    // Adiciona o tabuleiro escolhido aos tabuleiros já utilizados
+    if(usedBoards[res[next].toString()] === undefined){
+        usedBoards[res[next].toString()] = 0;
+    }
+    usedBoards[res[next].toString()] += 1;
+    // Troca p pelo escolhido e reavalia a soma
+    p = res[next];
+    p_sum = sum_city_block(p);
+    it += 1;
 }
 
-obj = [
-    [8,7,0],
-    [2,4,6],
-    [1,5,3]
-];
-
-let res = greedy_depth_one(obj);
-console.table(obj);
-console.table(res.board);
-console.log(res.it, "iterações");
-
-/*const queue = new MinQueue();
-queue.push(1, 2);
-queue.push(1, 3);
-console.log(queue)*/
+console.table(p)
+console.log(it, "iterações")
